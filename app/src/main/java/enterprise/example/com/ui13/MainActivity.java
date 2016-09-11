508891f7963cd8704478106e3fc8bcca2deee0b9
Atlanta11 <forever.friends.1980@gmail.com>
2016-09-11 22:13:32 +0200

    Initial commit

---
+++ app/src/main/java/enterprise/example/com/ui13/MainActivity.java
@@ -0,0 +1,17 @@
+package enterprise.example.com.ui13;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setContentView(new SingleTouchEventView(this, null));
+
+    }
+
+
+}
